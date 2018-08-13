6b456dda5bfa7c7c3bf3b0f9d0e9f371e256096f
master <284309510@qq.com>
2018-08-13 22:00:40 +0800

    提交日志，必须写上，不能空提交

--- MyWebProject/src/org/westos/demo/ServletDemo.java
+++ MyWebProject/src/org/westos/demo/ServletDemo.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletDemo")
+@WebServlet(name = "ServletDemo",value = "/demo")
 public class ServletDemo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("hehehhe");
